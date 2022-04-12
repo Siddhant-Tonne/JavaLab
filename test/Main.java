35698ca99a003bead45582127f2c922866ae1645
Siddhant-Tonne <siddhanttonne@gmail.com>
2022-04-12 22:17:43 +0530

    Modified the code little bit

--- test/Main.java
+++ test/Main.java
@@ -5,31 +5,33 @@ import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Main {
-
+    static LinkedList<helper> ll = new LinkedList<>();
     public static void main(String[] args) {
-        contacts c = new contacts("",0);
+        contacts c = new contacts("", 0);
         c.createContacts();
-	    tester();
+        tester();
     }
 
-    private static void tester(){
-        LinkedList<helper> ll = new LinkedList<>();
+    private static void tester() {
+
         helper h1 = new helper(9234343);
         helper h2 = new helper(5436544);
         helper h3 = new helper(8342784);
         helper h4 = new helper(6324243);
         helper h5 = new helper(9277482);
 
-        ll.add(h1);
-        ll.add(h2);
-        ll.add(h3);
-        ll.add(h4);
-        ll.add(h5);
+
+        add(h1);
+        add(h2);
+        add(h3);
+        add(h4);
+        add(h5);
+
 
         System.out.println("Missed call button pressed!!");
-        int choice ;
+        int choice;
         Scanner sc = new Scanner(System.in);
-        while(true){
+        while (true) {
             System.out.println();
             System.out.println("The missed call list currently is ");
             System.out.println(ll);
@@ -37,19 +39,19 @@ public class Main {
             choice = sc.nextInt();
             sc.nextLine();
             System.out.println();
-            switch(choice){
+            switch (choice) {
                 case 1:
                     System.out.println(ll);
                     break;
 
                 case 2:
                     Iterator<helper> i = ll.iterator();
-                    int quit ;
-                    while(i.hasNext()){
+                    int quit;
+                    while (i.hasNext()) {
                         System.out.println("Enter 1 to go to next missed call 2 to quit");
                         quit = sc.nextInt();
                         sc.nextLine();
-                        if(quit == 2)
+                        if (quit == 2)
                             break;
                         System.out.println(i.next());
 
@@ -75,4 +77,12 @@ public class Main {
             }
         }
     }
+
+    private static void add(helper h){
+        if(ll.size() == 10){
+            ll.remove(0);
+        }
+        ll.add(h);
+    }
+
 }
